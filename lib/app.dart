3f0e0f74cee4c0c5b3ccc52b4e2a219f61adfe9e
Noman Ali <132977453+space551@users.noreply.github.com>
2024-12-20 11:42:47 +0500

    Dependencies added

---
+++ lib/app.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Expense Tracker',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
+        useMaterial3: true,
+      ),
+      home: const Column(
+        children: [
+          Text("Hello World")
+        ],
+      ),
+    );
+  }
+}
