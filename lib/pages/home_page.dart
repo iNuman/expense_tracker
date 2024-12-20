4d22827ecf4e051e750dbeba9feb3e833343f134
Noman Ali <132977453+space551@users.noreply.github.com>
2024-12-20 17:42:51 +0500

    adding widgets to home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -15,7 +15,7 @@ class HomePage extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            TotalExpensesWidget(),
+            // TotalExpensesWidget(),
             SizedBox(height: 14),
             ExpenseFilterWidget(),
             SizedBox(height: 14),
